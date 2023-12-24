5138da77fcb7b9c5659c644682dc49da8194901d
warlock071603@gmail.com <warlock071603@gmail.com>
2023-12-24 14:57:06 +0800

    Updated info page for novel

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
-  
-  const newNovelContainer = $('#new-container');
-  
-  async function getNovels(){
-    // Get json response
-    const response = await fetch('data.json');
-    
-    // Check response if OK
-    if(!response.ok){
-      throw new Error('Failed to fetch data. Please try again!');
-    }
-    
-    const data = await response.json();
-    return data;
-    
-  }
-  
-  // Get novels on response
-  getNovels()
-  .then(data => {
-    
-     const dataResponse = data.data;
-     
-     dataResponse.forEach((items) => {
-       
-       const itemsDisplay = "<div class='new1'><img src='"+items.cover+"' alt='Cover' loading='lazy'/><h3 id='title'>"+items.title+"</h3><div class='item-info'><span>"+items.type+"</span></div></div>";
-       newNovelContainer.append(itemsDisplay);
-       
-     });
-     
-    
-  })
-  .catch(error => console.log(error));
-  
-  
+$(document).ready(function() {
+
+   const newNovelContainer = $('#new-container');
+
+   async function getNovels() {
+      // Get json response
+      const response = await fetch('data.json');
+
+      // Check response if OK
+      if (!response.ok) {
+         throw new Error('Failed to fetch data. Please try again!');
+      }
+
+      const data = await response.json();
+      return data;
+
+   }
+
+   // Get novels on response
+   getNovels()
+      .then(data => {
+
+         const dataResponse = data.data;
+
+         dataResponse.forEach((items) => {
+
+            const itemsDisplay = "<div class='new1'><img src='" + items.cover + "' alt='Cover' loading='lazy'/><h3 id='title'>" + items.title + "</h3><div class='item-info'><span>" + items.type + "</span></div></div>";
+            newNovelContainer.append(itemsDisplay);
+
+         });
+
+
+      })
+      .catch(error => console.log(error));
+
+
 });
---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+   
+   $('#bars').on('click', function() {
+     if (document.getElementById('navbar').style.left === '-150px') {
+        document.getElementById('navbar').style.left = '0';
+     } else {
+        document.getElementById('navbar').style.left = '-150px';
+     }
+  });
+   
+});
