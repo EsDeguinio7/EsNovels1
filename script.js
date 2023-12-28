4214c2e712ff6263c41e4ea6061cfd837f85890c
WarLock03 <150971007+WarLock03@users.noreply.github.com>
2023-12-28 10:57:20 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
-   
-   $('#bars').on('click', function() {
-     if (document.getElementById('navbar').style.left === '-150px') {
-        document.getElementById('navbar').style.left = '0';
-     } else {
-        document.getElementById('navbar').style.left = '-150px';
-     }
-  });
-   
-  $('#new-container').on('click','.content', function(){
-      
-      
-      window.location.href = $(this).attr('data-src');
-   });
-  $('#recommend-container').on('click','.content', function(){
-      
-      
-      window.location.href = $(this).attr('data-src');
-   });
-  $('#favourite-container').on('click','.content', function(){
-      
-      
-      window.location.href = $(this).attr('data-src');
-   });
-   
+document.addEventListener("DOMContentLoaded", function () {
+    $("#bars").on("click", function () {
+        if (document.getElementById("navbar").style.left === "-150px") {
+            document.getElementById("navbar").style.left = "0";
+        } else {
+            document.getElementById("navbar").style.left = "-150px";
+        }
+    });
+
+    $("#new-container").on("click", ".content", function () {
+        window.location.href = $(this).attr("data-src");
+    });
+    $("#recommend-container").on("click", ".content", function () {
+        window.location.href = $(this).attr("data-src");
+    });
+    $("#favourite-container").on("click", ".content", function () {
+        window.location.href = $(this).attr("data-src");
+    });
 });
